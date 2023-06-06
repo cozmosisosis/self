63186ae909a12011e18a04311a45cd5788c9367c
cozmosisosis <110356275+cozmosisosis@users.noreply.github.com>
2023-06-06 21:05:18 +0000

    test

--- test/functions.py
+++ test/functions.py
@@ -14,3 +14,4 @@ def human():
     """angry man talking"""
     name = input("whats your name:")
     print("you sound real stupid MR." + name + "\n")
+    print(input("You think you are smart? ") + "well you sound like a swell guy")
