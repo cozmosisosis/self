fdd8f9f5e7cff1ae2bdbbedd6fc514aa4f6e2675
cozmosisosis <110356275+cozmosisosis@users.noreply.github.com>
2023-11-17 18:42:00 +0000

    Created static folder and made styles css file

--- test/mylist/app.py
+++ test/mylist/app.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World and all its people!</p>"
+
+@app.route("/a")
+def ahello_world():
+    return "<p>AAAAAAAAAAAAAAAAA!</p>"
