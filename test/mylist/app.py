92ce2702cfbcfbb281106a43553635d58b340bfb
cozmosisosis <110356275+cozmosisosis@users.noreply.github.com>
2023-11-17 21:16:38 +0000

    Save Work

--- test/mylist/app.py
+++ test/mylist/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, flash, render_template, redirect, sessions, request
+import os
+import logging
+from flask import Flask, flash, render_template, redirect, session, request
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+logged_in = False
 
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
 
-    # if logged on
-    return render_template("index.html")
-    # else redirect to login
+    if logged_in:
+        return render_template("index.html")
+    
+    return redirect("/login")
         
 
 
@@ -27,6 +31,8 @@ def login():
 
     if request.method == 'GET':
         return render_template("login.html")
+    logged_in = True
+    
     return redirect("/")
 
 
