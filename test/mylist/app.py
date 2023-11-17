27dd199dbc31a1125b2b96e867bf67610200a3a8
cozmosisosis <110356275+cozmosisosis@users.noreply.github.com>
2023-11-17 19:41:57 +0000

    Set up flash message functionality

--- test/mylist/app.py
+++ test/mylist/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, redirect, sessions
+from flask import Flask, flash, render_template, redirect, sessions, request
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 @app.route("/")
 def index():
-    return render_template("/main_template.html")
+    return render_template("/index.html")
 
 @app.route("/a")
 def a():
     return "<p>AAAAAAAAAAAAAAAAA!</p>"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    flash('Not a valid path. Sorry! \nBut Anyways   ')
+    return redirect("/")
