ebf52c54b38dc44dc36676b5d205f7de77210fd4
cozmosisosis <110356275+cozmosisosis@users.noreply.github.com>
2023-11-17 20:54:08 +0000

    Created login path

--- test/mylist/app.py
+++ test/mylist/app.py
@@ -6,16 +6,33 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    if request.method == 'GET':
-        return render_template("login.html")
+
+    # if logged on
     return render_template("index.html")
+    # else redirect to login
         
 
-@app.route("/a")
-def a():
-    return "<p>AAAAAAAAAAAAAAAAA!</p>"
+
+@app.route("/registration", methods=['POST', 'GET'])
+def registration():
+
+    if request.method == 'GET':
+        return render_template("registration.html")
+    return redirect("/")
+
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+
+    if request.method == 'GET':
+        return render_template("login.html")
+    return redirect("/")
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
+
     flash('Not a valid path. Sorry! But Anyways   ')
     return redirect("/")
