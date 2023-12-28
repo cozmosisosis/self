45bc6bbe1ab3dba95e246e27bf93490827f48fae
cosmos <cosmos1998@gmail.com>
2023-12-28 15:27:36 -0800

    test

--- test/mylist/static/script_mylist_maintemplate.js
+++ test/mylist/static/script_mylist_maintemplate.js
@@ -1,5 +1,16 @@
 console.log("Main template js file loaded cloned and trying to push checking for changes")
 
+
+
+
+
+// Test
+
+
+
+
+
+
 // Need to test and understand
 
 // Other things to check for changes
