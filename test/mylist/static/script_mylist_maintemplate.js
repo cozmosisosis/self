4f3a2f04876e805de3e2da09240a5ee06d73fb80
cosmos <cosmos1998@gmail.com>
2023-12-28 15:22:12 -0800

    Testing pushing from clone

--- test/mylist/static/script_mylist_maintemplate.js
+++ test/mylist/static/script_mylist_maintemplate.js
@@ -1,1 +1,5 @@
-console.log("Main template js file loaded cloned and trying to push")
+console.log("Main template js file loaded cloned and trying to push checking for changes")
+
+
+
+// Other things to check for changes
